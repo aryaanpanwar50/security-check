9b81e4b7b143325d3e377d5b5b6913a2f2a3c2da
Aryaan007-sudo <blankgaming75@gmail.com>
2025-05-05 15:27:34 +0530

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
 
         setLoading(true);
         try {
-            const res = await axios.post(`http://localhost:5000/api/scan/${scanType}`, 
+            const res = await axios.post(`https://security-check-qgnx.onrender.com/api/scan/${scanType}`, 
                 { url },
                 { timeout: 30000 }
             );
